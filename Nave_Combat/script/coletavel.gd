09771377dfbf21ecb4c29d36480e3842447375c0
joão Vitor oliveira Rodrigues <joao.rodrigues@sou.inteli.edu.br>
2023-11-27 19:50:14 -0300

    blink

--- Nave_Combat/script/coletavel.gd
+++ Nave_Combat/script/coletavel.gd
@@ -2,7 +2,17 @@ extends Node2D
 
 var vel = 100
 
+var is_shaking = true
+var shake_duration = 1.3  # Duração da vibração em segundos
+var shake_amplitude = 5.0  # Amplitude da vibração (o quanto a tela vai tremer)
+
 func area_limite(obj,k):
 	if(position.y > get_viewport().size.y + k.y or position.x > get_viewport().size.x-(k.x) or position.x < -(k.x)):
 		obj.set_deferred("disabled", true)
 		queue_free()
+
+
+func start_shake():
+	if is_shaking:
+		var random_offset = Vector2(rand_range(-shake_amplitude+self.position.x, shake_amplitude+self.position.x), rand_range(-shake_amplitude+self.position.y, shake_amplitude+self.position.y))
+		self.position = random_offset
--- Nave_Combat/script/power_ip_impulso.gd
+++ Nave_Combat/script/power_ip_impulso.gd
@@ -1,12 +1,20 @@
 extends "res://script/coletavel.gd"
 
 # Called when the node enters the scene tree for the first time.
+var blink_timer := Timer.new()
+
 func _ready():
-	pass # Replace with function body.
+	pass
 
+func _blink():
+	# Inverter o valor da propriedade modulate para alternar entre visível e invisível
+	if self.modulate == Color(1, 1, 1, 1):
+		self.modulate = Color(1, 1, 1, 0)  # Tornar o objeto invisível
+	else:
+		self.modulate = Color(1, 1, 1, 1)  # Tornar o objeto visível
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	_blink()
 	position.y += (vel*delta)+Global.nave.Coeficiente
 	area_limite(self,Vector2(90,90))
 
